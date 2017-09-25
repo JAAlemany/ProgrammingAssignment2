f1ba040bfe2a96842c3ba61d41e2a1da9bc5f0ca
JAAlemany <31870899+JAAlemany@users.noreply.github.com>
2017-09-24 20:50:41 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,11 @@
 ## Put comments here that give an overall description of what your
 ## functions do.
 
-## Write a short comment describing this function - This "function" is being created from exsisting functions that created a matrix:
-"makeVector", "cacheVector" from supplied code. The below 2 functions are: 1) used to create the inverse of the matrix, and have an assigned value
-in a different enviornment (hence the <<- operator; 
-makeCacheMatrix <- function(x = matrix()) { ##name of matrix function
+## 1. Write a short comment describing this function - This "function" is being created from exsisting functions that created a matrix:
+"makeVector", "cacheVector" from supplied code. The below "makeCacheMatriz" function is used to create the inverse of the matrix, 
+and have an assigned value in a different enviornment (hence the <<- operator; 
+
+ makeCacheMatrix <- function(x = matrix()) { 
  inv = NULL ##
   set = function(y) { 
     x <<- y ## the <<- operator is used to # use `to assign a value to an object in an environment, but different from the current one 
@@ -18,9 +19,8 @@ makeCacheMatrix <- function(x = matrix()) { ##name of matrix function
 
 
 
-## Write a short comment describing this function - 2) this functionis able to cache computes the inverse of the “matrix” returned by 
-"makeCacheMatrix".
- ## Return a matrix that is the inverse of 'x'
+## 2. Write a short comment describing this function - 2) this function is able to cache computes the inverse of the “matrix” returned by 
+"makeCacheMatrix" as the function"cachemean" - thus, it returns a matrix that is the inverse of 'x'
            
 cachemean <- function(x, ...) {
   m <- x$getmean()
